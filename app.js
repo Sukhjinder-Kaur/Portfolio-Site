040390a3e65d84fdd6836793bd554a90b54fba0e
Sukhjinder-Kaur <sukhmanyu@gmail.com>
2020-10-11 18:02:27 -0400

    Added documentation to app.js

--- app.js
+++ app.js
@@ -4,32 +4,36 @@
      Student ID: 301087895
      Date: 10/10/2020
 */
-
-//Installed third party packages
+/*This  is the main files which links all the site structure together */
+//Installed third party packages and requires for linking the dependencies we need to run the project
 let  createError = require('http-errors');
 let  express = require('express');
 let  path = require('path');
-let  cookieParser = require('cookie-parser');
-let  logger = require('morgan');
+let  cookieParser = require('cookie-parser');//pakages to used for package prespectives
+let  logger = require('morgan');//pakages to used for package prespectives
 
+//Routers
 let  indexRouter = require('./routes/index');
-let  usersRouter = require('./routes/users');
+let  usersRouter = require('./routes/users');// not being used
 
+// for creating an instance of the express application itself
 let  app = express();
 
-// view engine setup
+// view engine setup to configure views using path.join
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');//express -e
 
+//activations
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));//static routes
 app.use(express.static(path.join(__dirname,'node_modules')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
