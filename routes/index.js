731282eb570ad7541349fad4debd41423a9f8046
Sukhjinder-Kaur <sukhmanyu@gmail.com>
2020-10-11 17:27:10 -0400

    Final updates

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 /*
      File name: index.js
+     File Type: Main Routes
      Student's name: Sukhjinder Kaur
      Student ID: 301087895
      Date: 10/10/2020
@@ -10,25 +11,19 @@ var router = express.Router();
 
 /* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Portfolio Site' });
+  res.render('index', { title: 'Portfolio Site', page: 'home' });
 });
-
-/* GET Home page. */
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Home' });
-});
-
 /* GET About Me page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.render('index', { title: 'About', page: 'about' });
 });
 /* GET Projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+  res.render('index', { title: 'Projects', page: 'projects' });
 });
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services' });
+  res.render('index', { title: 'Services', page: 'services' });
 });
 
 /* GET Contact Me page. */
